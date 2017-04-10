8e4f8127a047b5933914b31ceff41c3df7c2cd0b
rimoka95 <vothihoabkdn@gmail.com>
2017-04-10 22:18:39 +0700

    NavigationGooglePlay_ code1

--- NavigationGooglePLay/app/src/main/java/rimoka/com/navigationgoogleplay/ArtistAdapter.java
+++ NavigationGooglePLay/app/src/main/java/rimoka/com/navigationgoogleplay/ArtistAdapter.java
@@ -1,9 +1,12 @@
 package rimoka.com.navigationgoogleplay;
         import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+        import android.text.TextUtils;
+        import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+        import android.widget.Filter;
+        import android.widget.Filterable;
+        import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,12 +15,13 @@ import java.util.ArrayList;
  * Created by HOARIMOKA on 3/28/2017.
  */
 
-public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistHolder> {
+public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistHolder>  implements Filterable {
 
     ArrayList<ArtistFragment.Artist> list;
     MainActivity context;
     private ListenSong listenSong;
     LayoutInflater inflater;
+    private ItemFilter mFilter=new ItemFilter();
     public ArtistAdapter(ArrayList<ArtistFragment.Artist> list, MainActivity context, ListenSong listen) {
         this.list = list;
         this.context = context;
@@ -49,7 +53,41 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistHold
         return list.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        // TODO Auto-generated method stub
+        if(mFilter==null) mFilter=new ItemFilter();
+        return mFilter;
+    }
+    private class ItemFilter extends Filter {
+        FilterResults result = new FilterResults();
 
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            String filterString = constraint.toString().trim();
+            if (!TextUtils.isEmpty(filterString)) {
+                ArrayList<ArtistFragment.Artist> nlist = new ArrayList<ArtistFragment.Artist> ();
+                for (ArtistFragment.Artist item : ArtistFragment.listArtist) {
+                    if (item.getArtist().toString().contains(filterString))
+                        nlist.add(item);
+                }
+                result.values = nlist;
+                result.count = nlist.size();
+            }
+            return result;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint,
+                                      FilterResults results) {
+
+            if (results != null && results.count > 0) {
+                list = (ArrayList<ArtistFragment.Artist>) result.values;
+                notifyDataSetChanged();
+            }
+        }
+    }
     protected class ArtistHolder extends RecyclerView.ViewHolder{
         private TextView TenNghesy;
         private TextView Soluongbai;
